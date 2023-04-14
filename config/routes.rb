251bb6f9ceb2b9058e2c772dd7ce3635de94d403
Rafael <rbrajao@gmail.com>
2023-04-14 17:34:22 -0300

    Ajuste de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,17 @@ Rails.application.routes.draw do
   resources :championships
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  #Defines friendly routes (brazil)
+  get 'times' => 'teams#index'
+  get 'apostas' => 'bets#index'
+  get 'divisoes' => 'divisions#index'
+  get 'rodadas' => 'rounds#index'
+  get 'campeonatos' => 'championships#index'
+  get 'confrontos' => 'clashes#index'
+  get 'competidor' => 'users#index'
+   
+
   # Defines the root path route ("/")
   root "home#index"
 end
